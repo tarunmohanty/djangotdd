c50cfad0e3997e318ef847196474bf29c6ef64a4
Tarun <tarunmohantys@gmail.com>
2020-07-27 09:17:49 +0530

    add app for lists with basic view with html return. updating functional tests for this case

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
 
 #home_page = None
 
-def home_page():
-    pass
+#def home_page(request):
+ #   pass
+
+
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
